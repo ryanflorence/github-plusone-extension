6c83f5aa029f50f3b240f17fa492a6f39631bdd1
Alex Friedman <afriedman@homeandstone.com>
2015-06-18 16:57:14 -0400

    Added downvotes

--- content_script.js
+++ content_script.js
@@ -1,70 +1,78 @@
-var comments = [].slice.call(document.querySelectorAll('.timeline-comment-wrapper'), 0);
-var upAvatars = [];
-var downAvatars = [];
-var alreadySeenUp = [];
-var alreadySeenDown = [];
+function buildVoteIcons() {
+  var comments = [].slice.call(document.querySelectorAll('.timeline-comment-wrapper'), 0);
+  var upAvatars = [];
+  var downAvatars = [];
+  var alreadySeenUp = [];
+  var alreadySeenDown = [];
 
+  function findUpVoter(comment) {
+    findVoter('+1', comment, upAvatars, alreadySeenUp);
+  }
 
+  function findDownVoter(comment) {
+    findVoter('-1', comment, downAvatars, alreadySeenDown);
+  }
 
-function findUpVoter(comment) {
-  findVoter('+1', comment, upAvatars, alreadySeenUp);
-}
+  function findVoter(voteText, comment, avatars, alreadySeen) {
+    var regex = new RegExp('^\\' + voteText);
+    var text = comment.querySelector('.comment-body').textContent.trim();
+    if (text.match(regex) || comment.querySelector('img[title=":' + voteText + ':"]')) {
+      var avatar = comment.querySelector('a').cloneNode(true);
+      var user = avatar.href;
+      if (alreadySeen.indexOf(user) < 0) {
+        avatars.push(avatar);
+        alreadySeen.push(user);
+      }
 
-function findDownVoter(comment) {
-  findVoter('-1', comment, downAvatars, alreadySeenDown);
-}
-
-function findVoter(voteText, comment, avatars, alreadySeen) {
-  var regex = new RegExp('^\\' + voteText);
-  var text = comment.querySelector('.comment-body').textContent.trim();
-  if (text.match(regex) || comment.querySelector('img[title=":' + voteText + ':"]')) {
-    var avatar = comment.querySelector('a').cloneNode(true);
-    var user = avatar.href;
-    if (alreadySeen.indexOf(user) < 0) {
-      avatars.push(avatar);
-      alreadySeen.push(user);
-    }
-
-    if (text.match(new RegExp('^\\' + voteText +'$')) || !text ) { // there wont be text if the comment is just a 👍
-      comment.style.display='none';
+      if (text.match(new RegExp('^\\' + voteText +'$')) || !text ) { // there wont be text if the comment is just a 👍
+        comment.style.display='none';
+      }
     }
   }
-}
 
-function appendVotes(avatars, icon) {
-  if (avatars.length > 0) {
-    var div = document.createElement('div');
-    div.className = 'flex-table gh-header-meta';
-    div.innerHTML = ''+
-      '<div class="flex-table-item">'+
-      '  <div class="state" style="background: hsl(215, 50%, 50%)">'+
-      '    <span class="octicon octicon-' + icon + '"></span>'+
-           avatars.length+
-      '  </div>'+
-      '</div>'+
-      '<div class="flex-table-item flex-table-item-primary"></div>';
+  function appendVotes(avatars, icon) {
+    if (avatars.length > 0) {
+      var div = document.createElement('div');
+      div.className = 'flex-table gh-header-meta';
+      div.innerHTML = ''+
+        '<div class="flex-table-item">'+
+        '  <div class="state" style="background: hsl(215, 50%, 50%)">'+
+        '    <span class="octicon octicon-' + icon + '"></span>'+
+             avatars.length+
+        '  </div>'+
+        '</div>'+
+        '<div class="flex-table-item flex-table-item-primary"></div>';
 
-    var avatarContainer = div.querySelector('.flex-table-item-primary');
-    avatarContainer.style.paddingTop = 0;
+      var avatarContainer = div.querySelector('.flex-table-item-primary');
+      avatarContainer.style.paddingTop = 0;
 
-    avatars.forEach(function(avatar) {
-      var img = avatar.querySelector('img');
-      img.className = '';
-      img.style.height = '26px';
-      img.style.width = '26px';
-      img.style.margin = '0 2px';
-      img.style.borderRadius = '3px';
-      avatarContainer.appendChild(avatar);
-    });
+      avatars.forEach(function(avatar) {
+        var img = avatar.querySelector('img');
+        img.className = '';
+        img.style.height = '26px';
+        img.style.width = '26px';
+        img.style.margin = '0 2px';
+        img.style.borderRadius = '3px';
+        avatarContainer.appendChild(avatar);
+      });
 
-    document.querySelector('.gh-header-show').appendChild(div);
+      document.querySelector('.gh-header-show').appendChild(div);
+    }
   }
+
+  comments.forEach(function(comment) {
+    findUpVoter(comment);
+    findDownVoter(comment);
+  });
+
+  appendVotes(upAvatars, 'thumbsup');
+  appendVotes(downAvatars, 'thumbsdown');
 }
 
-comments.forEach(function(comment) {
-  findUpVoter(comment);
-  findDownVoter(comment);
+var observer = new MutationObserver(function() {
+  buildVoteIcons();
 });
 
-appendVotes(upAvatars, 'thumbsup');
-appendVotes(downAvatars, 'thumbsdown');
+observer.observe(document.querySelector('div.js-discussion'), {childList: true});
+
+buildVoteIcons();

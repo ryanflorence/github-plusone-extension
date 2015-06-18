da798630377d5c89c4a3f6a4ee3568640e559659
Eli Golding <egolding@qualitybath.com>
2015-06-18 16:37:42 -0400

    Added MutationObserver to rerun extension on change This is to remove the need to reload the page when either adding a comment or when a new  comment gets added by someone else.

--- content_script.js
+++ content_script.js
@@ -1,46 +1,55 @@
-var comments = [].slice.call(document.querySelectorAll('.timeline-comment-wrapper'), 0);
-var avatars = [];
-var alreadySeen = [];
-comments.forEach(function(comment) {
-  var text = comment.querySelector('.comment-body').textContent.trim();
-  if (text.match(/^\+1/) || comment.querySelector('img[title=":+1:"]')) {
-    var avatar = comment.querySelector('a').cloneNode(true);
-    var user = avatar.href;
-    if (alreadySeen.indexOf(user) < 0) {
-      avatars.push(avatar);
-      alreadySeen.push(user);
+function removePlusOnes() {
+  var comments = [].slice.call(document.querySelectorAll('.timeline-comment-wrapper'), 0);
+  var avatars = [];
+  var alreadySeen = [];
+  comments.forEach(function (comment) {
+    var text = comment.querySelector('.comment-body').textContent.trim();
+    if(text.match(/^\+1/) || comment.querySelector('img[title=":+1:"]')) {
+      var avatar = comment.querySelector('a').cloneNode(true);
+      var user = avatar.href;
+      if(alreadySeen.indexOf(user) < 0) {
+        avatars.push(avatar);
+        alreadySeen.push(user);
+      }
+      if(text.match(/^\+1$/) || !text) { // there wont be text if the comment is just a 👍
+        comment.style.display = 'none';
+      }
     }
-    if (text.match(/^\+1$/) || !text ) { // there wont be text if the comment is just a 👍
-      comment.style.display='none';
-    }
-  }
-});
-
-if (avatars.length > 0) {
-  var div = document.createElement('div');
-  div.className = 'flex-table gh-header-meta';
-  div.innerHTML = ''+
-    '<div class="flex-table-item">'+
-    '  <div class="state" style="background: hsl(215, 50%, 50%)">'+
-    '    <span class="octicon octicon-thumbsup"></span>'+
-         avatars.length+
-    '  </div>'+
-    '</div>'+
-    '<div class="flex-table-item flex-table-item-primary"></div>';
-
-  var avatarContainer = div.querySelector('.flex-table-item-primary');
-  avatarContainer.style.paddingTop = 0;
-
-  avatars.forEach(function(avatar) {
-    var img = avatar.querySelector('img');
-    img.className = '';
-    img.style.height = '26px';
-    img.style.width = '26px';
-    img.style.margin = '0 2px';
-    img.style.borderRadius = '3px';
-    avatarContainer.appendChild(avatar);
   });
 
-  document.querySelector('.gh-header-show').appendChild(div);
+  if(avatars.length > 0) {
+    var div = document.createElement('div');
+    div.className = 'flex-table gh-header-meta';
+    div.innerHTML = '' +
+        '<div class="flex-table-item">' +
+        '  <div class="state" style="background: hsl(215, 50%, 50%)">' +
+        '    <span class="octicon octicon-thumbsup"></span>' +
+        avatars.length +
+        '  </div>' +
+        '</div>' +
+        '<div class="flex-table-item flex-table-item-primary"></div>';
+
+    var avatarContainer = div.querySelector('.flex-table-item-primary');
+    avatarContainer.style.paddingTop = 0;
+
+    avatars.forEach(function (avatar) {
+      var img = avatar.querySelector('img');
+      img.className = '';
+      img.style.height = '26px';
+      img.style.width = '26px';
+      img.style.margin = '0 2px';
+      img.style.borderRadius = '3px';
+      avatarContainer.appendChild(avatar);
+    });
+
+    document.querySelector('.gh-header-show').appendChild(div);
+  }
 }
 
+var observer = new MutationObserver(function(mutations) {
+  removePlusOnes();
+});
+
+observer.observe(document.querySelector('div.js-discussion'), {childList: true});
+
+removePlusOnes();
